d3eea0fcb9bd9179c77b7b1a4878f39fa2e0f708
Duc Duy <ducduyhoang5@gmail.com>
2024-09-13 09:44:47 +0700

    Intitial commit

---
+++ Lecture_1.cpp
@@ -0,0 +1,15 @@
+/*
+Chapter 1: Introduction
+1. Course Description
++) Aim and scopes
+- This course provides fundamentals of programming techniques,
+implemented with C++ programming language.
++ Structure programming
++ Object-oriented programming
++ Real-time programming and typical data structures
++ Generic programming
+- You will also learn the essential knowledge, including:
++ Memory management, pointers, preprcessor macros;
++ How to find bugs;...
+
+*/
